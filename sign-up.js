5c071589eb380f378b39d2a3a508e2c0c738a59c
Cy <ogbangcyprian360@gmail.com>
2024-03-15 22:13:50 +0100

    Project completed. yet to add Readme and Comments

---
+++ sign-up.js
@@ -0,0 +1,33 @@
+function submit() {
+    const inputs = document.getElementById("emailAddress");
+    const error = document.querySelector(".errorMSG");
+    const signUP = document.querySelector(".container");
+    const Successful = document.querySelector(".successMSG");
+    const fillIN = document.getElementById("fills");
+
+    const mailAddress = inputs.value;
+
+    if (mailAddress === "") {
+        console.log("empty");
+        error.innerHTML = "Valid email required";
+        error.style.display = "block";
+    } else {
+        console.log("filled");
+        fillIN.innerHTML = mailAddress;
+        signUP.style.display = "none";
+        Successful.style.display = "block"
+    }
+}
+
+function dismiss() {
+    const signUP = document.querySelector(".container");
+    const Successful = document.querySelector(".successMSG");
+    const inputs = document.getElementById("emailAddress");
+
+
+    console.log("return");
+    Successful.style.display = "none";
+    signUP.style.display = "block";
+
+    inputs.value = "";
+}
